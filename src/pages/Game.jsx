8916f089c984d3563174092918e9c4ded10d6907
Santhosh Kumar <rvinothini@yahoo.co.in>
2024-03-10 19:28:34 +0000

    function created to display answers dynamically

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -2,16 +2,34 @@ import {useState,useEffect} from 'react';
 import  questions from '../data/CountryCapitals.json';
 
 
+const shuffleAnswer = question => {
+    const answer = [question.correct, ...question.incorrect];
+
+    for (let i=0; i< answer.length;i++){
+        const j= Math.floor(Math.random()*i);
+        const k= answer[i];
+        answer[i]=answer[j];
+        answer[j]=k;
+    }
+
+    return answer;
+}
+
+
 
 function Game(){
 
     const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
     const[currentQuestion, setCurrentQuestion] = useState(questions[0]);
+    const[answers,setAnswer]=useState([]);
 
     useEffect(()=>{
         const question = questions[currentQuestionIndex];
         setCurrentQuestion(question);
+        setAnswer(shuffleAnswer(question));
     },[currentQuestion]);
+
+
 
 
 
@@ -22,15 +40,19 @@ return (<>
             </div>
             <div>
                 <h4>{currentQuestion.question}</h4>
+                <ul>
+                {answers.map((answer, index) => (
+                            <li key={index}>{answer}</li>
+                        ))}
+                </ul>
+
             </div>
+
 
 
          </section>
 
        </>)
-
-
-
 
 }
 
