a4e292041ef3cb8574643ca2e3eb848315b2a1b4
Santhosh Kumar <rvinothini@yahoo.co.in>
2024-03-10 18:45:47 +0000

    gamepage updated with quiz questions

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -1,8 +1,9 @@
 import {useState,useEffect} from 'react';
+import  questions from '../data/CountryCapitals.json';
 
 
 
-function Game({questions}){
+function Game(){
 
     const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
     const[currentQuestion, setCurrentQuestion] = useState(questions[0]);
@@ -16,7 +17,13 @@ function Game({questions}){
 
 return (<>
          <section>
-            
+            <div>               
+                 {currentQuestionIndex+1}/{questions.length}
+            </div>
+            <div>
+                <h4>{currentQuestion.question}</h4>
+            </div>
+
 
          </section>
 
