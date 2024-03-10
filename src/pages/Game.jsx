8d8aeeb1678813e45c7ad4526264b63fd0afb8db
Santhosh Kumar <rvinothini@yahoo.co.in>
2024-03-10 18:17:01 +0000

    SampleDatafile created

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -1,6 +1,29 @@
 import {useState,useEffect} from 'react';
 
-function Game(){
+
+
+function Game({questions}){
+
+    const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+    const[currentQuestion, setCurrentQuestion] = useState(questions[0]);
+
+    useEffect(()=>{
+        const question = questions[currentQuestionIndex];
+        setCurrentQuestion(question);
+    },[currentQuestion]);
+
+
+
+return (<>
+         <section>
+            
+
+         </section>
+
+       </>)
+
+
+
 
 }
 
