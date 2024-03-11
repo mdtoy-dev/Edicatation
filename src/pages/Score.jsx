85689b776e74ed90aa731c8f21a0e40fb4a472dc
web3allday <rhenq4@gmail.com>
2024-03-11 11:33:35 +0000

    added points and show score live

--- src/pages/Score.jsx
+++ src/pages/Score.jsx
@@ -1,12 +1,18 @@
-import React from "react"
-
+import React from "react";
 
 function Score({ scoreCount }) {
   return (
-    <>
-      <div>Your Score: {scoreCount}</div>
-    </>
-  )
+    <div className="flex items-center justify-center h-screen">
+      <div className="bg-gray-100 p-8 rounded-md shadow-md">
+        <div className="text-4xl font-bold mb-4">
+          Congratulations, you scored
+        </div>
+        <div className="text-6xl text-center font-extrabold text-blue-500">
+          {scoreCount} Points
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Score
+export default Score;
