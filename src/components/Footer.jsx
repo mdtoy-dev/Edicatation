81d1480b4a08fb6533a2e8fe8657aff70f7d7a9d
hazalsigic <hazalsigic@hotmail.com>
2024-03-14 22:12:46 +0000

    homepage changed, Navbar explore and score buttons hidden untill name entry

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 function Footer() {
     return (
-        <footer id="footer" className="bg-cyan-600 h-10 text-white text-center p-3">
+        <footer id="footer" className="bg-cyan-500 h-10 text-cyan-50 text-center p-3">
             <div>
-                <p>Berk Rhen Vino Hazal Francis</p>
+                <p className=" drop-shadow-2xl">Berk Rhen Vino Hazal Francis</p>
             </div>
         </footer>
     )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,15 +4,10 @@ import NameEntry from "../components/NameEntry"
 
 const Home = () => {
   return (
-    <div className="flex flex-col justify-center items-center">
-      <NameEntry />
-      <img src={logo} alt="logo" width={400} />
-      <Link
-        to="/gamecategory"
-        className="bg-orange-500 hover:bg-cyan-700 text-white font-bold py-2 px-10 rounded"
-      >
-        Discover the topics
-      </Link>
+    <div className="flex flex-col sm:flex-row">
+      <img src={logo} alt="logo" className="w-1/3 m-auto drop-shadow-2xl" />
+            <NameEntry className="w-2/3"/>
+      
     </div>
   )
 }
